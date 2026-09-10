7249777c14bc4c98df1bc6e690c7556c746c5f36
Dominik-Alge <dominik.alge@bluewin.ch>
2026-09-10 20:29:46 +0200

    Refactor Team component structure and content

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -27,8 +27,7 @@ export default function Team() {
               src="/rina.jpg"
               alt="Alge Rina"
               class>
-            <div className="p-8">
-
+              
               <h3 className="text-3xl font-bold text-slate-900">
                 Alge Rina
               </h3>
@@ -78,7 +77,6 @@ export default function Team() {
             <img
               src="/barbara.jpg"
               alt="Barbara Morciano-Alvarez>
-            <div className="p-8">
 
               <h3 className="text-3xl font-bold text-slate-900">
                 Morciano-Alvarez Barbara
