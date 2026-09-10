555f760b5d971b679c836271f64201cb730b6351
Dominik-Alge <dominik.alge@bluewin.ch>
2026-09-10 14:20:02 +0200

    Refactor App component to include multiple sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import Hero from "./components/Hero";
+import Problems from "./components/Problems";
+import Services from "./components/Services";
+import Approach from "./components/Approach";
+import WhyKinavita from "./components/WhyKinavita";
+import Team from "./components/Team";
+import References from "./components/References";
+import Process from "./components/Process";
+import Contact from "./components/Contact";
+
 function App() {
   return (
-    <div>
-      <h1>Kinavita</h1>
-      <p>Über Bewegung im Dialog bleiben.</p>
-    </div>
+    <>
+      <Hero />
+      <Problems />
+      <Services />
+      <Approach />
+      <WhyKinavita />
+      <Team />
+      <References />
+      <Process />
+      <Contact />
+    </>
   );
 }
 
 export default App;
+``
