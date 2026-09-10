5c75a7fbcd75c0b7b0aa481c0fb689a169cbcbaa
Dominik-Alge <dominik.alge@bluewin.ch>
2026-09-10 14:01:56 +0200

    Add team data structure in content.js

---
+++ src/content.js
@@ -0,0 +1,19 @@
+export const team = [
+  {
+    id: 1,
+    name: "Rina",
+    role: "Kinaesthetics Trainerin Stufe 2",
+    description:
+      "Begleitung von Institutionen und Menschen im Alltag.",
+    image: "/placeholder-person.jpg"
+  },
+
+  {
+    id: 2,
+    name: "Name 2",
+    role: "Kinaesthetics Trainerin",
+    description:
+      "Praxisbegleitung, Ergonomie und Gesundheitsförderung.",
+    image: "/placeholder-person.jpg"
+  }
+];
