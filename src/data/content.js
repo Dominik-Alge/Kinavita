87255092f6a244fd170a1fc77f48226a28af17e7
Dominik-Alge <dominik.alge@bluewin.ch>
2026-09-10 14:44:55 +0200

    Add team data to content.js

---
+++ src/data/content.js
@@ -0,0 +1,16 @@
+export const team = [
+  {
+    id: 1,
+    name: "Rina",
+    role: "Kinaesthetics Trainerin Stufe 2",
+    description:
+      "Begleitung von Institutionen, Teams und Einzelpersonen."
+  },
+  {
+    id: 2,
+    name: "Name folgt",
+    role: "Kinaesthetics Trainerin",
+    description:
+      "Praxisbegleitung, Gesundheitsförderung und Bewegungslernen."
+  }
+];
