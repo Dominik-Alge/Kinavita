22ea22464ccc821a9cb06ed4081975cf7116c3dc
Dominik-Alge <dominik.alge@bluewin.ch>
2026-09-10 18:43:09 +0200

    Add WhyKinavita component to App

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,9 @@ function App() {
       <Hero />
       <WhoAreYou />
       <Problems />
+      <WhyKinavita />
       <Services />
       <Approach />
-      <WhyKinavita />
       <Team />
       <References />
       <Process />
